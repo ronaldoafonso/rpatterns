57053c438d8c345ed995d1caeebf94ea23bc2c2e
Ronaldo Afonso <ronaldo@ronaldoafonso.com.br>
2020-03-06 12:56:29 -0300

    Add better tests for Increment and Decrement methods of the Singleton Design Pattern.

--- singleton.go
+++ singleton.go
@@ -1,19 +1,34 @@
+// Package singleton ... Singleton Design Pattern
 package singleton
 
+var uniqueSingleton *Singleton
+
+// Singleton ... An example of a singleton struct. It should be unique.
 type Singleton struct {
 	value int
 }
 
-var uniqueSingleton *Singleton
-
-func GetSingleton() *Singleton {
+// NewSingleton ... Returns a singleton var. It should return the same
+// singleton var every time it was called.
+func NewSingleton() *Singleton {
 	if uniqueSingleton == nil {
-		uniqueSingleton = new(Singleton)
+		uniqueSingleton = &Singleton{}
 	}
 	return uniqueSingleton
 }
 
+// Increment ... Increment the value of singleton to 2 at maximum.
 func (s *Singleton) Increment() int {
-	s.value++
+	if s.value != 2 {
+		s.value++
+	}
+	return s.value
+}
+
+// Decrement ... Decrement the value of singleton to 0 at minimum.
+func (s *Singleton) Decrement() int {
+	if s.value != 0 {
+		s.value--
+	}
 	return s.value
 }
--- singleton_test.go
+++ singleton_test.go
@@ -2,24 +2,42 @@ package singleton
 
 import "testing"
 
-func TestGetSingleton(t *testing.T) {
-	singleton1 := GetSingleton()
-	if singleton1 == nil {
-		t.Error("GetSingleton returning nil instead of a singleton.")
+func TestSingleton(t *testing.T) {
+	s1 := NewSingleton()
+	if s1 == nil {
+		t.Error("NewSingleton returning nil instead of a singleton.")
 	}
 
-	singleton2 := GetSingleton()
-	if singleton1 != singleton2 {
-		t.Error("GetSingleton returning not a unique singleton.")
+	for i := 0; i < 10; i++ {
+		if s := NewSingleton(); s != s1 {
+			t.Error("NewSingleton returning not a unique singleton.")
+		}
 	}
 
-	valueOne := singleton1.Increment()
-	if valueOne != 1 {
-		t.Errorf("Increment should return 1 at first invocation, but returned %d.\n", valueOne)
+	s2 := NewSingleton()
+	tests := []struct {
+		name string
+		do   func() int
+		want int
+	}{
+		{"s1.Increment", s1.Increment, 1},
+		{"s2.Increment", s2.Increment, 2},
+		{"s1.Decrement", s1.Decrement, 1},
+		{"s2.Decrement", s2.Decrement, 0},
+		{"s1.Decrement", s1.Decrement, 0},
+		{"s2.Increment", s2.Increment, 1},
+		{"s1.Increment", s1.Increment, 2},
+		{"s2.Increment", s2.Increment, 2},
 	}
 
-	valueTwo := singleton2.Increment()
-	if valueTwo != 2 {
-		t.Errorf("Increment should return 2 at second invocation, but returned %d.\n", valueTwo)
+	for n, test := range tests {
+		got := test.do()
+		if got != test.want {
+			t.Errorf("Test %v - Function: %v error. Want: %v, got: %v.",
+				n+1,
+				test.name,
+				test.want,
+				got)
+		}
 	}
 }
